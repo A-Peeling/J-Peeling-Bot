fef93d01259cf2759f468dc3a795a19c5b29aa39
stophman1 <11683358+stophman1@users.noreply.github.com>
2020-11-22 19:32:47 -0600

    Ping now back, but in not black.

--- src/main/java/com/apeeling/jpeeling/Bot.java
+++ src/main/java/com/apeeling/jpeeling/Bot.java
@@ -25,10 +25,16 @@ package com.apeeling.jpeeling;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * This is a pretty epic bot ngl
@@ -37,27 +43,30 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  * @author stophman1
  */
 
-public class Bot {
-    private static final String BOT_TOKEN = ("./res/config.properties");
-
-    public static void main(String[] arguments) throws Exception {
-        JDA api = JDABuilder.createDefault(BOT_TOKEN).build();
+public class Bot extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException, IOException {
+        Properties prop = PropertiesController.readPropertiesFile("./res/config.properties");
+        if (prop.getProperty("token").equals("INSERT_TOKEN")) {
+            System.out.print("You need to setup the bot in order to use it. /res/config.properties");
+            System.exit(0);
+        }
+        // args[0] should be the token
+        // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
+        // All other events will be disabled.
+        JDABuilder.createLight(prop.getProperty("token"), GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+                .addEventListeners(new Bot())
+                .setActivity(Activity.playing(prop.getProperty("doin")))
+                .build();
     }
 
-    public class MyListener extends ListenerAdapter {
-        @Override
-        public void onMessageReceived(MessageReceivedEvent event) {
-            if (event.getAuthor().isBot()) return;
-            // We don't want to respond to other bot accounts, including ourself
-            Message message = event.getMessage();
-            String content = message.getContentRaw();
-            // getContentRaw() is an atomic getter
-            // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
-            if (content.equals("!ping")) {
-                MessageChannel channel = event.getChannel();
-                channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
-            }
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().equals("!ping")) {
+            MessageChannel channel = event.getChannel();
+            long time = System.currentTimeMillis();
+            channel.sendMessage("Pong!") /* => RestAction<Message> */
+                    .queue(response /* => Message */ -> response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue());
         }
     }
-
 }
